b89f3174f3623e0cf777f655d918bc921778ebac
scriptertoufiq <scriptertoufiq@gmail.com>
2026-02-23 12:37:26 +0600

    rabbitmq connection build

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'database'),
+    'default' => env('QUEUE_CONNECTION', 'rabbitmq'),
 
     /*
     |--------------------------------------------------------------------------
@@ -87,6 +87,37 @@ return [
                 'database',
                 'deferred',
             ],
+        ],
+
+        'rabbitmq' => [
+            'driver'     => 'rabbitmq',
+            'queue'      => env('RABBITMQ_QUEUE', 'default'),
+            'connection' => PhpAmqpLib\Connection\AMQPLazyConnection::class,
+
+            'hosts' => [
+                [
+                    'host'     => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port'     => env('RABBITMQ_PORT', 5672),
+                    'user'     => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost'    => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+
+            'options' => [
+                'ssl_options' => [
+                    'cafile'      => env('RABBITMQ_SSL_CAFILE'),
+                    'local_cert'  => env('RABBITMQ_SSL_LOCALCERT'),
+                    'local_key'   => env('RABBITMQ_SSL_LOCALKEY'),
+                    'verify_peer' => env('RABBITMQ_SSL_VERIFY_PEER', true),
+                    'passphrase'  => env('RABBITMQ_SSL_PASSPHRASE'),
+                ],
+                'queue' => [
+                    'job' => VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Jobs\RabbitMQJob::class,
+                ],
+            ],
+
+            'worker' => env('RABBITMQ_WORKER', 'default'),
         ],
 
     ],
