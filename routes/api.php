a38b5dc7d3356d2542eeecbc69de46ea064d6a99
scriptertoufiq <scriptertoufiq@gmail.com>
2026-02-23 14:17:59 +0600

    api set for blog

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,5 @@
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::resource('blogs', BlogController::class);
 
